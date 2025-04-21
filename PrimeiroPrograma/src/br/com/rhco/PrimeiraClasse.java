c90294725c14de43d016c7a401134995b665c985
rodolfo hernani <rodolfohco@hotmail.com>
2025-04-21 19:04:53 -0300

    Pacote inicial

---
+++ PrimeiroPrograma/src/br/com/rhco/PrimeiraClasse.java
@@ -0,0 +1,10 @@
+package br.com.rhco;
+
+public class PrimeiraClasse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello Word !");
+	}
+
+}
